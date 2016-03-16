e617f45dbcd1f388ba295b7da0bfcfc21721ef52
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2016-03-16 14:21:12 -0400

    added like animation and jQuery

--- Soup-To-Bits/1-Interactive-Website/part-1/Scripts/app.js
+++ Soup-To-Bits/1-Interactive-Website/part-1/Scripts/app.js
@@ -1,1 +1,14 @@
-// app.js
+/*-----------------------------------------------
+*   DOCUMENT READY
+-----------------------------------------------*/
+
+$(document).ready(function(){
+    $('.js-like').on('click', function (event){
+        event.preventDefault();
+        
+        $(this)
+            .text('Liked!')
+            .closest('.news-item')
+            .addClass('is-liked'); 
+    });
+});

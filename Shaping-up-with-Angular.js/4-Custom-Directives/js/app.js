406d69cf6d7a1490a364fb76ef15b76f4fef5117
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2016-03-23 23:56:05 -0400

    added productDescription directive

--- Shaping-up-with-Angular.js/4-Custom-Directives/js/app.js
+++ Shaping-up-with-Angular.js/4-Custom-Directives/js/app.js
@@ -33,6 +33,13 @@
       this.review = {};
     };    
   });
+    
+   app.directive('productDescription', function(){
+    return{
+      restrict: 'E',
+      templateUrl: 'product-description.html'
+    };
+  });
 
   var gems = [
     {

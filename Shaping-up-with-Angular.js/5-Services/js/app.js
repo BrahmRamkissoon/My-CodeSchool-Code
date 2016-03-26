532207531af0c6981cae39d668336586e9e838a7
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2016-03-26 15:24:23 -0400

    5.2 - refactored product related directives to new module product-directives

--- Shaping-up-with-Angular.js/5-Services/js/app.js
+++ Shaping-up-with-Angular.js/5-Services/js/app.js
@@ -5,69 +5,12 @@
         this.products = gems;
     });
 
-    app.directive('productTabs', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'product-tabs.html',
-            controller: function () {
-                this.tab = 1;
-
-                this.setTab = function (tab) {
-                    this.tab = tab;
-                };
-
-                this.isSet = function (tab) {
-                    return (this.tab === tab);
-                };
-            },
-            controllerAs: 'tab'
-        };
-
-    });
-
-
-    app.directive('productGallery', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'product-gallery.html',
-            controller: function () {
-                this.current = 0;
-                this.setCurrent = function (imageNumber) {
-                    this.current = imageNumber || 0;
-                };
-            },
-            controllerAs: 'gallery'
-        };
-    });
-
     app.controller('ReviewController', function () {
         this.review = {};
 
         this.addReview = function (product) {
             product.reviews.push(this.review);
             this.review = {};
-        };
-    });
-
-    app.directive('productDescription', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'product-description.html'
-        };
-    });
-
-    app.directive("productReviews", function () {
-        return {
-            restrict: 'E',
-            templateUrl: "product-reviews.html"
-        };
-    });
-
-
-    app.directive("productSpecs", function () {
-        return {
-            restrict: 'A',
-            templateUrl: "product-specs.html"
         };
     });
 

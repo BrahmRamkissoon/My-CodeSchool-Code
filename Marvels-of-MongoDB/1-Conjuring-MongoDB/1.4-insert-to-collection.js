13eb92c484155949da06034dc125e3d5256f0951
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2016-03-28 00:45:58 -0400

    1.4 - inserted document to wands collection

---
+++ Marvels-of-MongoDB/1-Conjuring-MongoDB/1.4-insert-to-collection.js
@@ -0,0 +1,10 @@
+/*
+We're proud of the shiny new wand we've just purchased, so let's add it to the wands collection. Our wand's name is "Dream Bender" and its creator is "Foxmond".
+
+Write a command to insert our wand into the wands collection.
+*/
+
+db.wands.insert({
+    "name": "Dream Bender",
+    "creator": "Foxmond"
+})
